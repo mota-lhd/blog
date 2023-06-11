3898e0997f4aea6fb77f8bfbc5abf0bcffcdcdd7
Elmouatassim Louhaidia <mota@kraggin.com>
2023-06-12 00:02:50 +0200

    refactor: iac for a and aaaa dns mappings

--- iac/modules/mappings/mappings.tf
+++ iac/modules/mappings/mappings.tf
@@ -29,39 +29,39 @@ resource "google_dns_record_set" "verif" {
 }
 
 locals {
-  tmp_data = [
+  record_types = ["A", "AAAA"]
+
+  records_by_subdomain = distinct(flatten([
     for index, mapping in google_cloud_run_domain_mapping.mappings : {
       subdomain = index
       data      = { for record in mapping.status[0].resource_records : record.type => record.rrdata... }
     }
-  ]
+  ]))
+
+  tmp_data = distinct(flatten([
+    for mapping in local.records_by_subdomain : [
+      for record_type in local.record_types : {
+        type      = record_type
+        subdomain = mapping.subdomain
+        data      = mapping.data[record_type]
+      }
+    ]
+  ]))
+
   records = {
-    for mapping in local.tmp_data : mapping.subdomain => mapping.data
+    for mapping in local.tmp_data : "${mapping.subdomain}-${mapping.type}" => mapping
   }
 }
 
-resource "google_dns_record_set" "a_records" {
+resource "google_dns_record_set" "records" {
   for_each = local.records
 
   project = var.project_id
-  name    = "${each.key}.${var.main_domain}."
-  type    = "A"
+  name    = "${each.value.subdomain}.${var.main_domain}."
+  type    = each.value.type
   ttl     = 300
 
   managed_zone = google_dns_managed_zone.zone.name
 
-  rrdatas = each.value["A"]
-}
-
-resource "google_dns_record_set" "aaaa_records" {
-  for_each = local.records
-
-  project = var.project_id
-  name    = "${each.key}.${var.main_domain}."
-  type    = "AAAA"
-  ttl     = 300
-
-  managed_zone = google_dns_managed_zone.zone.name
-
-  rrdatas = each.value["AAAA"]
+  rrdatas = each.value.data
 }
