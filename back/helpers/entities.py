7ada060dd879f1771ce6d668ec0a0cf9c4676529
Elmouatassim Louhaidia <mota.lhd@gmail.com>
2023-07-01 20:04:18 +0100

    Add files to ignore file
    ignore: ruff cache files

--- back/helpers/entities.py
+++ back/helpers/entities.py
@@ -1,43 +1,39 @@
 import os
+from typing import Iterator, List
 
-from typing import List, Iterator
-from google.cloud.datastore import (Client as StorageClient, Entity, Query, Key)
-
-from google.auth import impersonated_credentials, default
+from google.auth import default, impersonated_credentials
 from google.auth.impersonated_credentials import Credentials
-
+from google.cloud.datastore import Client as StorageClient
+from google.cloud.datastore import Entity, Key, Query
 
 post_titles: List[str] = [
-    u'dhaulagiri-2019-01',
-    u'dhaulagiri-2019-02',
-    u'dhaulagiri-2019-03',
-    u'dhaulagiri-2019-04',
-    u'dhaulagiri-2019-05',
-    u'echappee-belle-2020-01',
-    u'pokhara-2020-01',
-    u'rollwaling-2019-01',
-    u'rollwaling-2019-02',
-    u'tech-blog-01'
+    "dhaulagiri-2019-01",
+    "dhaulagiri-2019-02",
+    "dhaulagiri-2019-03",
+    "dhaulagiri-2019-04",
+    "dhaulagiri-2019-05",
+    "echappee-belle-2020-01",
+    "pokhara-2020-01",
+    "rollwaling-2019-01",
+    "rollwaling-2019-02",
+    "tech-blog-01",
 ]
 source_credentials, project_id = default()
-project_id: str = os.environ['PROJECT_ID']
-main_sa: str = os.environ['MAIN_SA']
+project_id: str = os.environ["PROJECT_ID"]
+main_sa: str = os.environ["MAIN_SA"]
 creds: Credentials = impersonated_credentials.Credentials(
     source_credentials=source_credentials,
     target_principal=(f"{main_sa}@{project_id}.iam.gserviceaccount.com"),
     target_scopes=["https://www.googleapis.com/auth/cloud-platform"],
 )
-storage: StorageClient = StorageClient(
-    project=project_id,
-    credentials=creds
-)
+storage: StorageClient = StorageClient(project=project_id, credentials=creds)
 query: Query = storage.query(kind="Article")
 results: Iterator = query.fetch()
-already_in: List[str] = [r['title'] for r in list(results)]
+already_in: List[str] = [r["title"] for r in list(results)]
 
 for post in post_titles:
     if post not in already_in:
-        key: Key = storage.key('Article')
+        key: Key = storage.key("Article")
         entity: Entity = Entity(key=key)
 
         entity["title"] = post
