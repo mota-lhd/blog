23187dddeefca40ff3066f0217e51f31417524f9
Elmouatassim Louhaidia <mota.lhd@gmail.com>
2024-01-29 12:48:40 +0100

    chore: use cloudrun-v2 within tf for the 3 services

--- iac/modules/cloud-run/service.tf
+++ iac/modules/cloud-run/service.tf
@@ -1,69 +1,68 @@
-resource "google_cloud_run_service" "service" {
+resource "google_cloud_run_v2_service" "service" {
   name     = var.service_name
   location = var.location
   project  = var.project_name
 
   template {
-    spec {
-      # https://cloud.google.com/run/docs/configuring/request-timeout
-      timeout_seconds = 15 * 60
+    service_account = var.runner_sa
 
-      container_concurrency = 3
-
-      containers {
-        image = var.image_name
-        ports {
-          name           = "http1"
-          protocol       = "TCP"
-          container_port = var.tcp_port
+    scaling {
+      max_instance_count = 3
+    }
+    containers {
+      image = var.image_name
+      ports {
+        name           = "http1"
+        container_port = var.tcp_port
+      }
+      startup_probe {
+        initial_delay_seconds = 5
+        timeout_seconds       = 15 * 60
+        period_seconds        = 10
+        failure_threshold     = 5
+        tcp_socket {
+          port = var.tcp_port
         }
-        dynamic "env" {
-          for_each = (try(var.options.env, null) == null ? {} : var.options.env)
-          content {
-            name  = env.key
-            value = env.value
-          }
+      }
+      liveness_probe {
+        http_get {
+          path = "/"
         }
-        dynamic "env" {
-          for_each = (try(var.options.env_from, null) == null ? {} : var.options.env_from)
-          content {
-            name = env.key
-            value_from {
-              secret_key_ref {
-                name = env.value.name
-                key  = env.value.key
-              }
+      }
+      dynamic "env" {
+        for_each = (try(var.options.env, null) == null ? {} : var.options.env)
+        content {
+          name  = env.key
+          value = env.value
+        }
+      }
+      dynamic "env" {
+        for_each = (try(var.options.env_from, null) == null ? {} : var.options.env_from)
+        content {
+          name = env.key
+          value_source {
+            secret_key_ref {
+              secret  = env.value.name
+              version = env.value.key
             }
           }
         }
-        resources {
-          limits = {
-            # Memory usage limit (per container)
-            # https://cloud.google.com/run/docs/configuring/memory-limits
-            memory = "512Mi"
-            # CPU usage limit
-            # https://cloud.google.com/run/docs/configuring/cpu
-            cpu = "1000m" # 1 vCPU
-          }
+      }
+      resources {
+        limits = {
+          # Memory usage limit (per container)
+          # https://cloud.google.com/run/docs/configuring/memory-limits
+          memory = "512Mi"
+          # CPU usage limit
+          # https://cloud.google.com/run/docs/configuring/cpu
+          cpu = "1000m" # 1 vCPU
         }
       }
-      service_account_name = var.runner_sa
-    }
-    metadata {
-      annotations = {
-        # Max instances
-        # https://cloud.google.com/run/docs/configuring/max-instances
-        "autoscaling.knative.dev/maxScale" = 10
-      }
     }
   }
-  metadata {
-    annotations = {
-      # For valid annotation values and descriptions, see
-      # https://cloud.google.com/sdk/gcloud/reference/run/deploy#--ingress
-      "run.googleapis.com/ingress" = "all"
-    }
-  }
+
+  ingress = "INGRESS_TRAFFIC_ALL"
+
   lifecycle {
     ignore_changes = [
       template[0].spec[0].containers[0].ports[0].protocol,
@@ -73,7 +72,7 @@ resource "google_cloud_run_service" "service" {
     ]
   }
   traffic {
-    percent         = 100
-    latest_revision = true
+    percent = 100
+    type    = "TRAFFIC_TARGET_ALLOCATION_TYPE_LATEST"
   }
 }
