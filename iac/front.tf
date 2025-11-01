ce4a0e52fc71b7a4c38e75a50e2dcddd27a361a8
elmouatassim <10708635+mota-lhd@users.noreply.github.com>
2025-11-01 12:07:56 +0100

    chores: cleanup (#97)

--- iac/front.tf
+++
@@ -1,5 +0,0 @@
-resource "google_service_account" "frontend_sa" {
-  project      = var.project_id
-  account_id   = "frontend-service-account"
-  display_name = "A service account used for frontend cloud run."
-}
