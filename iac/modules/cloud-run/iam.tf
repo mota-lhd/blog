1e84d2f9c1703f54dea790f2ec26acfd44391abc
Elmouatassim <elmouatassim.louhaidia@pm.me>
2025-11-17 10:23:35 +0100

    chores: remove iac part from repo

--- iac/modules/cloud-run/iam.tf
+++
@@ -1,7 +0,0 @@
-resource "google_cloud_run_v2_service_iam_member" "invoker" {
-  project  = var.project_name
-  location = var.location
-  name     = google_cloud_run_v2_service.service.name
-  role     = "roles/run.invoker"
-  member   = "allUsers"
-}
